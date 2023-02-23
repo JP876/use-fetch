043d83bc329a15b0ed7cb02fb35a82e7f1a90743
Josip <popovicjosip867@gmail.com>
2023-02-23 18:45:40 +0100

    Added response as second argument to func function

--- index.js
+++ index.js
@@ -1,89 +1,97 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from "react";
 
-const isArrayEmpty = arr => Array.isArray(arr) && arr.length === 0;
+const isArrayEmpty = (arr) => Array.isArray(arr) && arr.length === 0;
 
 const useFetch = () => {
-    const [response, setResponse] = useState({});
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState({ error: false, msg: null });
-    const [options, setOptions] = useState({});
+	const [response, setResponse] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState({ error: false, msg: null });
 
-    const handleReduce = useCallback((optionsArr, signal) => {
-        return optionsArr
-            .reduce((promiseChain, currentFunc, i) => {
-                return promiseChain.then(data => {
-                    if (currentFunc?.url) {
-                        let options = currentFunc?.options;
-                        let { url } = currentFunc;
+	const [options, setOptions] = useState({});
 
-                        if (!currentFunc.options) {
-                            options = {};
-                        }
+	const handleReduce = useCallback((optionsArr, signal) => {
+		return optionsArr
+			.reduce((promiseChain, currentFunc, i) => {
+				return promiseChain.then((data) => {
+					if (currentFunc?.url) {
+						let options = currentFunc?.options;
+						let { url } = currentFunc;
 
-                        return fetch(url, { ...options, signal })
-                            .then(res => Promise.all([res.text(), res]))
-                            .then(([text, res]) => {
-                                try {
-                                    let data = JSON.parse(text);
+						if (!currentFunc.options) {
+							options = {};
+						}
 
-                                    if (!res.ok) {
-                                        return Promise.reject(data);
-                                    }
-                                    setResponse(prevState => ({
-                                        ...prevState,
-                                        [currentFunc?.id || i]: data,
-                                    }));
-                                    if (optionsArr.length - 1 === i) {
-                                        setIsLoading(false);
-                                    }
+						return fetch(url, { ...options, signal })
+							.then((res) => Promise.all([res.text(), res]))
+							.then(([text, res]) => {
+								try {
+									let data = JSON.parse(text);
 
-                                    return Promise.resolve(data);
-                                } catch (err) {
-                                    //console.log('Received text');
-                                    if (!res.ok) {
-                                        return Promise.reject();
-                                    }
-                                    return Promise.resolve();
-                                }
-                            });
-                    } else if (typeof currentFunc?.func === 'function') {
-                        if (optionsArr.length - 1 === i) {
-                            setIsLoading(false);
-                        }
-                        if (typeof currentFunc.func === 'function') {
-                            currentFunc.func(data);
-                        }
-                        return Promise.resolve(data);
-                    }
-                });
-            }, Promise.resolve())
-            .catch(err => {
-                setIsLoading(false);
-                setResponse(false);
-                setError({ error: true, msg: err });
-            });
-    }, []);
+									if (!res.ok) {
+										return Promise.reject(data);
+									}
+									setResponse((prevState) => ({
+										...prevState,
+										[currentFunc?.id || i]: data,
+									}));
 
-    const doFetch = useCallback(options => {
-        if (!isArrayEmpty(options)) {
-            setOptions(options);
-            setIsLoading(true);
-            setError({ error: false, msg: null });
-            setResponse({});
-        }
-    }, []);
+									if (optionsArr.length - 1 === i) {
+										setIsLoading(false);
+									}
 
-    useEffect(() => {
-        if (!isLoading) return;
+									return Promise.resolve({ data, res });
+								} catch (err) {
+									//'Received text';
+									if (!res.ok) {
+										return Promise.reject();
+									}
+									return Promise.resolve({ data: text, res });
+								}
+							});
+					} else if (typeof currentFunc?.func === "function") {
+						if (optionsArr.length - 1 === i) {
+							setIsLoading(false);
+						}
+						if (data && Object.keys(data).length !== 0) {
+							currentFunc.func(data?.data, data?.res);
+						}
+						return Promise.resolve(data);
+					} else {
+						if (optionsArr.length - 1 === i) {
+							setIsLoading(false);
+						}
 
-        let controller = new AbortController();
+						return Promise.resolve(data);
+					}
+				});
+			}, Promise.resolve())
+			.catch((err) => {
+				setIsLoading(false);
+				setResponse(false);
+				setError({ error: true, msg: err });
+			});
+	}, []);
 
-        handleReduce(options, controller.signal);
+	const doFetch = useCallback((options) => {
+		if (!isArrayEmpty(options)) {
+			setOptions(options);
+			setIsLoading(true);
+			setError({ error: false, msg: null });
+			setResponse({});
+		}
+	}, []);
 
-        return () => controller.abort();
-    }, [isLoading, options, handleReduce]);
+	useEffect(() => {
+		if (!isLoading) return;
 
-    return { response, error, isLoading, doFetch };
+		let controller = new AbortController();
+
+		handleReduce(options, controller.signal);
+
+		return () => controller.abort();
+	}, [isLoading, options, handleReduce]);
+
+	return { response, error, isLoading, doFetch };
 };
 
 export default useFetch;
