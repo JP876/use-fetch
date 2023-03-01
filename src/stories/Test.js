c53a613fdbe0499ce4e5b34511beb8c4c6eb84bf
Josip <popovicjosip867@gmail.com>
2023-03-01 17:58:10 +0100

    Added storybook

---
+++ src/stories/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <>
+            <div>Yoo</div>
+        </>
+    );
+};
+
+export default Test;
---
+++ src/stories/useFetch.stories.js
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react';
+import Test from './Test';
+
+const stories = storiesOf('useFetch', module);
+
+stories.add('useFetch', Test);
